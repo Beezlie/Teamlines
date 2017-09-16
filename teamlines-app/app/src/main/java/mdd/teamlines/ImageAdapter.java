c0cc3613afb9ae1a9538d429b0f50ef978053351
Beezlie <matthew.d.dias@gmail.com>
2017-09-16 11:10:42 -0400

    Added Team object Moved team name, league to strings.xml Created new adapter for grid view

--- teamlines-app/app/src/main/java/mdd/teamlines/ImageAdapter.java
+++
@@ -1,8 +0,0 @@
-package mdd.teamlines;
-
-/**
- * Created by matt on 8/20/17.
- */
-
-public class ImageAdapter {
-}
---
+++ teamlines-app/app/src/main/java/mdd/teamlines/TeamsAdapter.java
@@ -0,0 +1,8 @@
+package mdd.teamlines;
+
+/**
+ * Created by matt on 9/15/17.
+ */
+
+public class TeamsAdapter {
+}
