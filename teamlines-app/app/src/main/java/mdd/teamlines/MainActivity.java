81dca1232c8e07c4453a9513d8f29e72f6ae29e4
Beezlie <matthew.d.dias@gmail.com>
2017-09-16 21:18:21 -0400

    added toolbar to main activity added sport icons to toolbar

--- teamlines-app/app/src/main/java/mdd/teamlines/MainActivity.java
+++ teamlines-app/app/src/main/java/mdd/teamlines/MainActivity.java
@@ -1,19 +1,29 @@
 package mdd.teamlines;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         GridView gridView = (GridView)findViewById(R.id.gridview);
 
         final TeamsAdapter teamsAdapter = new TeamsAdapter(this, teams);
